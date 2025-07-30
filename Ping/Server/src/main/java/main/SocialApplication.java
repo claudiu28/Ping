7ce13ff9338f63411950a8fdbd5dd7e31cd0c079
Claudiu <84350723+claudiu28@users.noreply.github.com>
2025-07-30 11:21:25 +0300

    Add .env

--- Ping/Server/src/main/java/main/SocialApplication.java
+++ Ping/Server/src/main/java/main/SocialApplication.java
@@ -1,11 +1,18 @@
 package main;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication(scanBasePackages = "main")
 public class SocialApplication {
     public static void main(String[] args) {
+        Dotenv dotenv = Dotenv.configure()
+                .ignoreIfMalformed()
+                .ignoreIfMissing()
+                .load();
+
+        dotenv.entries().forEach(entry-> System.setProperty(entry.getKey(), entry.getValue()));
         SpringApplication.run(SocialApplication.class, args);
     }
 }
